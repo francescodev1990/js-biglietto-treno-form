a34d0a545d69609f7b7581444568944c9a802380
Your Name <almannofrancesco@gmail.com>
2022-11-29 09:51:51 +0100

    js modified

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-let btnGenerate = document.getElementById("generate");
-btnGenerate.addEventListener("click");
+//SCOMPOSIZIONE DEL PROBLEMA//
+
+//INSERISCI I KM//
+//INSERISCI L'ETA'//
+//LA TARIFFA AL KM SARA' DI 0,21//
+//I MINORENNI AVRANNO UNO SCONTO PARI AL 20% (<18)//
+//GLI OVER 65 AVRANNO UNO SCONTO PARI AL 40% (>65)//
+//ALTRIMENTI NON CI SARA' NESSUNO SCONTO//
+
+//PASSAGGI DA ESEGUIRE//
+
+//REALIZZARE CAMPO INPUT PER I KM//
+//REALIZZARE CAMPO INPUT PER L'ETA'//
+//CALCOLO DEL PREZZO TOTALE TRAMITE PRESSIONE DI UN PULSANTE//
+//STAMPARE A VIDEO I RISULTATI//
+
+let km = prompt('Per favore inserisci i km che devi percorrere');
+console.log(km);
+
+let eta = prompt('Per favore inserisci adesso la tua età, servirà per il calcolo finale del costo');
+console.log(eta);
