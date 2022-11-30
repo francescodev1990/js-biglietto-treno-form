0bafa689480f6623fdb707931aaf8e3ad05d5651
Your Name <almannofrancesco@gmail.com>
2022-11-30 18:22:33 +0100

    discount

--- js/script.js
+++ js/script.js
@@ -14,15 +14,47 @@
 //CALCOLO DEL PREZZO TOTALE TRAMITE PRESSIONE DI UN PULSANTE//
 //STAMPARE A VIDEO I RISULTATI//
 
+
+
+//CLICK PULSANTE GENERA//
+//INSERIMENTO EVENTI INPUT TEXT E SELECT//
+
+
 const price_button = document.getElementById('generate');
-price_button.addEventListener('click', function(){
+price_button.addEventListener('click', function () {
     let name_surname = document.getElementById('name')
-    .value;
+        .value;
     console.log(name_surname);
-    let chilometri_percorsi = document.getElementById('km')
-    .value;
-    console.log(chilometri_percorsi);
+
+    let chilometri = document.getElementById('km')
+        .value;
+    console.log(chilometri);
+
     let age = document.getElementById('eta')
-    .value;
+        .value;
     console.log(age);
+
+    let prezzo = 0.21 * chilometri;
+    console.log(prezzo);
+
+    
+
+
+
+    if (eta == 'minorenne') {
+        prezzo = 0.21 * chilometri * 0.8;
+    }
+    else if (eta == 'over65') {
+        prezzo = 0, 21 * chilometri * 0.6;
+    }
+    else {
+        prezzo = 0.21 * chilometri;
+    }
+    console.log(prezzo)
 });
+
+
+
+
+
+
